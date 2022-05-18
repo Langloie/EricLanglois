b01bcdcbb459b750d4517fc160103cec736b5e2a
Langloie <Langloiea@gmail.com>
2022-05-17 21:06:53 -0400

    updated express homework

--- Homework/Week6Day4Lab/pass-it-around/server.js
+++ Homework/Week6Day4Lab/pass-it-around/server.js
@@ -9,10 +9,15 @@ app.get('/', (req, res)=>{
     res.send("99 Bottles of beer on the wall <a href= 'localhost:4000/98'> take one down, pass it around</a>");
 })
 
-//add show route
+
 app.get('/:number_of_bottles', (req, res)=>{
-    res.send(req.params.number_of_bottles-1,"Bottles of beer on the wall <a href='localhost:4000/:number_of_bottles'>take one down, pass it around</a>");
+    if(req.params.number_of_bottles>0){
+        res.send(req.params.number_of_bottles+` Bottles of beer on the wall <a href='localhost:4000/${req.params.number_of_bottles-1}'>take one down, pass it around</a>`);
+    }else{
+        res.send(req.params.number_of_bottles+" Bottles of beer on the wall");
+    }
 });
 
 
 app.listen(PORT);
+
